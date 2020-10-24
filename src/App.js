d9e6bc07ec949c42326e782d6c6c86d22ccdef59
git-nandor <nandor@leadforward.hu>
2020-10-24 18:48:36 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import config from "./config";
+import "./styles.css";
+
+export default function App() {
+  const [searchValue, setSearchValue] = useState(() => "");
+  const [searchInfo, setSearchInfo] = useState(() => "Start typing for search");
+  const [requestedData, setrequestedData] = useState(() => "");
+  const [selectedBreed, setSelectedBreed] = useState(() => "");
+  const appConfig = config();
+
+  const clearUi = () => {
+    setSelectedBreed("");
+    setrequestedData("");
+  };
+
+  const handleInputChange = (event) => {
+    clearUi();
+    if (event.target.value !== "") {
+      setSearchInfo("Loading...");
+    } else {
+      setSearchInfo("Start typing for search");
+    }
+
+    setTimeout(
+      (event) => {
+        setSearchValue(event.target.value);
+      },
+      500,
+      event
+    );
+  };
+
+  const handleBreedSelect = (event) => {
+    setSelectedBreed(requestedData[event.target.id]);
+  };
+
+  useEffect(() => {
+    const handleRequest = async () => {
+      try {
+        const response = await axios.get(`${appConfig.url}/?q=${searchValue}`, {
+          headers: { "x-api-key": appConfig.key }
+        });
+
+        if (response.status === 200) {
+          setrequestedData(response.data);
+          setSearchInfo(`Results for ${searchValue}: `);
+        }
+      } catch (error) {
+        setSearchInfo(`Sorry ¯\\_(ツ)_/¯ something went wrong!`);
+      }
+    };
+
+    if (searchValue !== "") {
+      handleRequest(searchValue);
+    }
+  }, [searchValue]);
+
+  const SearchInfo = () => {
+    return <div> {searchInfo} </div>;
+  };
+
+  const SelectedBreed = () => {
+    return (
+      <div>
+        <h2> {selectedBreed.name} </h2>
+        <div> {selectedBreed.description} </div>
+      </div>
+    );
+  };
+
+  const SearchResult = () => {
+    //searchInfo = 'Start typing for search'; // ???? how to set with useRef, triggered by searchValue changes
+    let resultElement = "";
+
+    if (requestedData !== "" && requestedData !== "error") {
+      const results = requestedData.map((result, index) => {
+        // ??? how to custom props... must full lowercase as: dataindex but...
+        return (
+          <button
+            key={result.id}
+            id={index}
+            dataindex={index}
+            onClick={handleBreedSelect}
+          >
+            {result.name}
+          </button>
+        );
+      });
+      resultElement = results;
+    } else if (requestedData === "error") {
+      resultElement = "";
+    }
+
+    return <div> {resultElement} </div>;
+  };
+
+  return (
+    <div className="App">
+      <h1>CodeSandbox React Practice</h1>
+      <h2>Lets search for cat breeds!</h2>
+      <div>
+        <form>
+          <label htmlFor="searchInput">Search: </label>
+          <input id="searchInput" onChange={handleInputChange} />
+        </form>
+        <br />
+        <hr />
+        <br />
+        <SearchInfo />
+        <SearchResult />
+        <SelectedBreed />
+      </div>
+    </div>
+  );
+}
---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default function config() {
+  return {
+    url:
+      "https://cors-anywhere.herokuapp.com/https://api.thecatapi.com/v1/breeds/search",
+
+    // Use it as the 'x-api-key' header when making any request to the API
+    key: "6b26bd94-ccb2-42e8-b3d5-ded0ff369ebb"
+  };
+}
